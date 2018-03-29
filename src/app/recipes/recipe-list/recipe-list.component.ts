0daf792624acd0ffe046000143a4e8a2cbad9643
Rafael Mendoza <rafael-a-mendoza@telecom.pt>
2018-03-29 17:01:50 -0300

    "Commit 2"

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Teste Name', 'Teste Recipe Desc',
+     'http://cdn-image.myrecipes.com/sites/default/files/peanut_chicken_and_sugar_snap_peas_with_noodles.jpg'),
+     new Recipe('A Teste Name', 'Teste Recipe Desc',
+     'http://cdn-image.myrecipes.com/sites/default/files/peanut_chicken_and_sugar_snap_peas_with_noodles.jpg')
+  ];
 
   constructor() { }
 
